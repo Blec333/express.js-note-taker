1ab40901e56bf713ad0095866a393c6d1376bb0c
BLEC333 <bleclair3@gmail.com>
2022-04-27 22:56:59 -0700

    add api post router

--- routes/notesApiRouter.js
+++ routes/notesApiRouter.js
@@ -12,6 +12,29 @@ router.get('/notes', (req, res) => {
 
 
 
+// POST Route for a new UX/UI note
+router.post('/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+
+
 
 
 module.exports = router;
