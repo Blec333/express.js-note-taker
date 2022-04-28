47ad4fb3c4260f13f9793ae85ac138682b756f15
BLEC333 <bleclair3@gmail.com>
2022-04-27 23:00:39 -0700

    add server.js middleware

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+//MIDDLEWARE:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Note Taker app is listening at http://localhost:${PORT}`);
